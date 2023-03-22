c2bc138d38e6dabee8424e28b29c7e6add868494
hecmas <lecter1729@gmail.com>
2023-03-22 19:55:10 +0100

    Working on the pairing

--- src/ellipticCurve.ts
+++ src/ellipticCurve.ts
@@ -1,10 +1,14 @@
-import { FiniteField } from "./finiteField";
 import { PrimeField } from "./primeField";
 import { ExtensionField } from "./extensionField";
 
-interface Point<U> {
-    x: U;
-    y: U;
+export interface PointOverFp {
+    x: bigint;
+    y: bigint;
+}
+
+export interface PointOverFq {
+    x: bigint[];
+    y: bigint[];
 }
 
 // /*
@@ -14,16 +18,14 @@ interface Point<U> {
 //     * 4·a³ + 27·b² != 0
 //     * p is prime
 //     */
-export class EllipticCurve<T extends FiniteField<U>, U> {
-    readonly a: U;
-    readonly b: U;
-    readonly F: T;
+export class EllipticCurveOverFp {
+    readonly a: bigint;
+    readonly b: bigint;
+    readonly Fp: PrimeField;
 
-    constructor(a: U, b: U, field: T) {
-        const four = field instanceof PrimeField ? 4n : [4n];
-        const ts = field instanceof PrimeField ? 27n : [27n];
-        const firstSummand = field.mul(four as U, field.exp(a, 3n));
-        const secondSummand = field.mul(ts as U, field.exp(b, 2n));
+    constructor(a: bigint, b: bigint, field: PrimeField) {
+        const firstSummand = field.mul(4n, field.exp(a, 3n));
+        const secondSummand = field.mul(27n, field.exp(b, 2n));
         const sum = field.add(firstSummand, secondSummand);
         if (field.eq(sum, field.zero)) {
             throw new Error("The curve is singular, choose another a and b");
@@ -31,7 +33,7 @@ export class EllipticCurve<T extends FiniteField<U>, U> {
 
         this.a = a;
         this.b = b;
-        this.F = field;
+        this.Fp = field;
     }
 
     // Public Accessors
@@ -39,28 +41,28 @@ export class EllipticCurve<T extends FiniteField<U>, U> {
         return null;
     }
 
-    // Check if a Point is the identity element
-    is_zero(P: Point<U>): boolean {
+    // Check if a point is the identity element
+    is_zero(P: PointOverFp): boolean {
         return P === this.zero;
     }
 
-    // Check that a point is on the curve defined by y² == x³ + a·x + b
-    is_on_curve(P: Point<U>): boolean {
+    // Check that a point is on the curve
+    is_on_curve(P: PointOverFp): boolean {
         if (this.is_zero(P)) {
             return true;
         }
 
-        const left_side = this.F.exp(P.y, 2n);
-        const right_side = this.F.add(
-            this.F.add(this.F.exp(P.x, 3n), this.F.mul(this.a, P.x)),
+        const left_side = this.Fp.exp(P.y, 2n);
+        const right_side = this.Fp.add(
+            this.Fp.add(this.Fp.exp(P.x, 3n), this.Fp.mul(this.a, P.x)),
             this.b
         );
 
-        return this.F.eq(left_side, right_side);
+        return this.Fp.eq(left_side, right_side);
     }
 
     // Basic Arithmetic
-    add(P: Point<U>, Q: Point<U>): Point<U> {
+    add(P: PointOverFp, Q: PointOverFp): PointOverFp {
         if (this.is_zero(P)) return Q;
 
         if (this.is_zero(Q)) return P;
@@ -72,34 +74,32 @@ export class EllipticCurve<T extends FiniteField<U>, U> {
             }
         }
 
-        let m: U;
-        const three = this.F instanceof PrimeField ? 3n : [3n];
-        const two = this.F instanceof PrimeField ? 2n : [2n];
+        let m: bigint;
         if (P.x === Q.x && P.y === Q.y) {
-            m = this.F.div(
-                this.F.add(this.F.mul(three as U, this.F.mul(P.x, P.x)), this.a),
-                this.F.mul(two as U, P.y)
+            m = this.Fp.div(
+                this.Fp.add(this.Fp.mul(3n, this.Fp.mul(P.x, P.x)), this.a),
+                this.Fp.mul(2n, P.y)
             );
         } else {
-            m = this.F.div(this.F.sub(Q.y, P.y), this.F.sub(Q.x, P.x));
+            m = this.Fp.div(this.Fp.sub(Q.y, P.y), this.Fp.sub(Q.x, P.x));
         }
 
-        const x = this.F.sub(this.F.sub(this.F.mul(m, m), P.x), Q.x);
-        const y = this.F.sub(this.F.mul(m, this.F.sub(P.x, x)), P.y);
+        const x = this.Fp.sub(this.Fp.sub(this.Fp.mul(m, m), P.x), Q.x);
+        const y = this.Fp.sub(this.Fp.mul(m, this.Fp.sub(P.x, x)), P.y);
         return { x, y };
     }
 
-    sub(P: Point<U>, Q: Point<U>): Point<U> {
+    sub(P: PointOverFp, Q: PointOverFp): PointOverFp {
         return this.add(P, this.neg(Q));
     }
 
-    neg(P: Point<U>): Point<U> {
+    neg(P: PointOverFp): PointOverFp {
         if (this.is_zero(P)) return this.zero;
 
-        return { x: this.F.mod(P.x), y: this.F.neg(P.y) };
+        return { x: this.Fp.mod(P.x), y: this.Fp.neg(P.y) };
     }
 
-    escalarMul(P: Point<U>, k: bigint): Point<U> {
+    escalarMul(P: PointOverFp, k: bigint): PointOverFp {
         if (k === 0n) return this.zero;
 
         if (k < 0n) {
@@ -119,17 +119,26 @@ export class EllipticCurve<T extends FiniteField<U>, U> {
         return R;
     }
 
+    embedding_degree(r: bigint): bigint {
+        let k = 1n;
+        while ((this.Fp.p**k - 1n) % r !== 0n) {
+            k += 1n;
+        }
+
+        return k;
+    }
+
     // Fix this
     // twist(P: PointFq, w2: bigint[], w3: bigint[]): PointFq {
     //     if (this.is_zero(P)) return this.zero;
 
     //     // Field isomorphism from Fp[X]/(X²) to Fp[X]/(X² - 18·X + 82)
     //     let xcoeffs = [
-    //         this.F.Fp.sub(P.x[0], this.F.Fp.mul(9n, P.x[1])),
+    //         this.Fp.Fp.sub(P.x[0], this.Fp.Fp.mul(9n, P.x[1])),
     //         P.x[1],
     //     ];
     //     let ycoeffs = [
-    //         this.F.Fp.sub(P.y[0], this.F.Fp.mul(9n, P.y[1])),
+    //         this.Fp.Fp.sub(P.y[0], this.Fp.Fp.mul(9n, P.y[1])),
     //         P.y[1],
     //     ];
     //     // Isomorphism into subfield of Fp[X]/(w¹² - 18·w⁶ + 82),
@@ -138,9 +147,306 @@ export class EllipticCurve<T extends FiniteField<U>, U> {
     //     let ny = [ycoeffs[0], 0n, 0n, 0n, 0n, 0n, ycoeffs[1]];
 
     //     // Divide x coord by w² and y coord by w³
-    //     let x = this.F.div(nx, w2);
-    //     let y = this.F.div(ny, w3);
+    //     let x = this.Fp.div(nx, w2);
+    //     let y = this.Fp.div(ny, w3);
 
     //     return { x, y };
     // }
 }
+
+// /*
+//     * Elliptic curve over Fq
+//     * y² = x³ + a·x + b
+//     * a, b are Fq elements
+//     * 4·a³ + 27·b² != 0
+//     * q is a prime power
+//     */
+export class EllipticCurveOverFq {
+    readonly a: bigint[];
+    readonly b: bigint[];
+    readonly Fq: ExtensionField;
+
+    constructor(a: bigint[], b: bigint[], field: ExtensionField) {
+        const firstSummand = field.mul([4n], field.exp(a, 3n));
+        const secondSummand = field.mul([27n], field.exp(b, 2n));
+        const sum = field.add(firstSummand, secondSummand);
+        if (field.eq(sum, field.zero)) {
+            throw new Error("The curve is singular, choose another a and b");
+        }
+
+        this.a = a;
+        this.b = b;
+        this.Fq = field;
+    }
+
+    // Public Accessors
+    get zero(): null {
+        return null;
+    }
+
+    // Check if a point is the identity element
+    is_zero(P: PointOverFq): boolean {
+        return P === this.zero;
+    }
+
+    // Check that a point is on the curve
+    is_on_curve(P: PointOverFq): boolean {
+        if (this.is_zero(P)) {
+            return true;
+        }
+
+        const left_side = this.Fq.exp(P.y, 2n);
+        const right_side = this.Fq.add(
+            this.Fq.add(this.Fq.exp(P.x, 3n), this.Fq.mul(this.a, P.x)),
+            this.b
+        );
+
+        return this.Fq.eq(left_side, right_side);
+    }
+
+    // Basic Arithmetic
+    add(P: PointOverFq, Q: PointOverFq): PointOverFq {
+        if (this.is_zero(P)) return Q;
+
+        if (this.is_zero(Q)) return P;
+
+        if (P.x === Q.x) {
+            if (P.y !== Q.y) {
+                // P = -Q
+                return this.zero;
+            }
+        }
+
+        let m: bigint[];
+        if (P.x === Q.x && P.y === Q.y) {
+            m = this.Fq.div(
+                this.Fq.add(this.Fq.mul([3n], this.Fq.mul(P.x, P.x)), this.a),
+                this.Fq.mul([2n], P.y)
+            );
+        } else {
+            m = this.Fq.div(this.Fq.sub(Q.y, P.y), this.Fq.sub(Q.x, P.x));
+        }
+
+        const x = this.Fq.sub(this.Fq.sub(this.Fq.mul(m, m), P.x), Q.x);
+        const y = this.Fq.sub(this.Fq.mul(m, this.Fq.sub(P.x, x)), P.y);
+        return { x, y };
+    }
+
+    sub(P: PointOverFq, Q: PointOverFq): PointOverFq {
+        return this.add(P, this.neg(Q));
+    }
+
+    neg(P: PointOverFq): PointOverFq {
+        if (this.is_zero(P)) return this.zero;
+
+        return { x: this.Fq.mod(P.x), y: this.Fq.neg(P.y) };
+    }
+
+    escalarMul(P: PointOverFq, k: bigint): PointOverFq {
+        if (k === 0n) return this.zero;
+
+        if (k < 0n) {
+            k = -k;
+            P = this.neg(P);
+        }
+
+        let R = P;
+        let binary = k.toString(2);
+        for (let i = 1; i < binary.length; i++) {
+            R = this.add(R, R);
+            if (binary[i] === "1") {
+                R = this.add(R, P);
+            }
+        }
+
+        return R;
+    }
+
+    embedding_degree(r: bigint): bigint {
+        let k = 1n;
+        while ((this.Fq.Fp.p**k - 1n) % r !== 0n) {
+            k += 1n;
+        }
+
+        return k;
+    }
+
+    // Fix this
+    // twist(P: PointFq, w2: bigint[], w3: bigint[]): PointFq {
+    //     if (this.is_zero(P)) return this.zero;
+
+    //     // Field isomorphism from Fp[X]/(X²) to Fp[X]/(X² - 18·X + 82)
+    //     let xcoeffs = [
+    //         this.Fq.Fp.sub(P.x[0], this.Fq.Fp.mul(9n, P.x[1])),
+    //         P.x[1],
+    //     ];
+    //     let ycoeffs = [
+    //         this.Fq.Fp.sub(P.y[0], this.Fq.Fp.mul(9n, P.y[1])),
+    //         P.y[1],
+    //     ];
+    //     // Isomorphism into subfield of Fp[X]/(w¹² - 18·w⁶ + 82),
+    //     // where w⁶ = X
+    //     let nx = [xcoeffs[0], 0n, 0n, 0n, 0n, 0n, xcoeffs[1]];
+    //     let ny = [ycoeffs[0], 0n, 0n, 0n, 0n, 0n, ycoeffs[1]];
+
+    //     // Divide x coord by w² and y coord by w³
+    //     let x = this.Fq.div(nx, w2);
+    //     let y = this.Fq.div(ny, w3);
+
+    //     return { x, y };
+    // }
+}
+
+
+// Tests
+// const Fp = new PrimeField(11n);
+// const E = new EllipticCurveOverFp(0n, 4n, Fp);
+// const r = 3n;
+// const k = E.embedding_degree(r);
+// console.log(k);
+
+// const Fp = new PrimeField(11n);
+// const E = new EllipticCurveOverFp(7n, 2n, Fp);
+// const r = 7n;
+// const k = E.embedding_degree(r);
+// console.log(k);
+
+// export class EllipticCurve<T extends FiniteField<U>, U> {
+//     readonly a: bigint;
+//     readonly b: bigint;
+//     readonly F: PrimeField;
+
+//     constructor(a: bigint, b: bigint, field: PrimeField) {
+//         const four = field instanceof PrimeField ? 4n : [4n];
+//         const ts = field instanceof PrimeField ? 27n : [27n];
+//         const firstSummand = field.mul(four as U, field.exp(a, 3n));
+//         const secondSummand = field.mul(ts as U, field.exp(b, 2n));
+//         const sum = field.add(firstSummand, secondSummand);
+//         if (field.eq(sum, field.zero)) {
+//             throw new Error("The curve is singular, choose another a and b");
+//         }
+
+//         this.a = a;
+//         this.b = b;
+//         this.F = field;
+//     }
+
+//     // Public Accessors
+//     get zero(): null {
+//         return null;
+//     }
+
+//     // Check if a Point is the identity element
+//     is_zero(P: PointOverFp): boolean {
+//         return P === this.zero;
+//     }
+
+//     // Check that a point is on the curve defined by y² == x³ + a·x + b
+//     is_on_curve(P: PointOverFp): boolean {
+//         if (this.is_zero(P)) {
+//             return true;
+//         }
+
+//         const left_side = this.F.exp(P.y, 2n);
+//         const right_side = this.F.add(
+//             this.F.add(this.F.exp(P.x, 3n), this.F.mul(this.a, P.x)),
+//             this.b
+//         );
+
+//         return this.F.eq(left_side, right_side);
+//     }
+
+//     // Basic Arithmetic
+//     add(P: PointOverFp, Q: PointOverFp): PointOverFp {
+//         if (this.is_zero(P)) return Q;
+
+//         if (this.is_zero(Q)) return P;
+
+//         if (P.x === Q.x) {
+//             if (P.y !== Q.y) {
+//                 // P = -Q
+//                 return this.zero;
+//             }
+//         }
+
+//         let m: bigint;
+//         const three = this.F instanceof PrimeField ? 3n : [3n];
+//         const two = this.F instanceof PrimeField ? 2n : [2n];
+//         if (P.x === Q.x && P.y === Q.y) {
+//             m = this.F.div(
+//                 this.F.add(this.F.mul(three as U, this.F.mul(P.x, P.x)), this.a),
+//                 this.F.mul(two as U, P.y)
+//             );
+//         } else {
+//             m = this.F.div(this.F.sub(Q.y, P.y), this.F.sub(Q.x, P.x));
+//         }
+
+//         const x = this.F.sub(this.F.sub(this.F.mul(m, m), P.x), Q.x);
+//         const y = this.F.sub(this.F.mul(m, this.F.sub(P.x, x)), P.y);
+//         return { x, y };
+//     }
+
+//     sub(P: PointOverFp, Q: PointOverFp): PointOverFp {
+//         return this.add(P, this.neg(Q));
+//     }
+
+//     neg(P: PointOverFp): PointOverFp {
+//         if (this.is_zero(P)) return this.zero;
+
+//         return { x: PrimeFieldhis.F.mod(P.x), y: PrimeFieldhis.F.neg(P.y) };
+//     }
+
+//     escalarMul(P: PointOverFp, k: bigint): PointOverFp {
+//         if (k === 0n) return this.zero;
+
+//         if (k < 0n) {
+//             k = -k;
+//             P = this.neg(P);
+//         }
+
+//         let R = P;
+//         let binary = k.toString(2);
+//         for (let i = 1; i < binary.length; i++) {
+//             R = this.add(R, R);
+//             if (binary[i] === "1") {
+//                 R = this.add(R, P);
+//             }
+//         }
+
+//         return R;
+//     }
+
+//     embedding_degree(): number {
+//         let k = 1
+//         while (this.F.exp(this.F.p, k) !== this.F.one) {
+//             k += 1
+//         }
+
+//         return k;
+//     }
+
+//     // Fix this
+//     // twist(P: PointFq, w2: bigint[], w3: bigint[]): PointFq {
+//     //     if (this.is_zero(P)) return this.zero;
+
+//     //     // Field isomorphism from Fp[X]/(X²) to Fp[X]/(X² - 18·X + 82)
+//     //     let xcoeffs = [
+//     //         this.F.Fp.sub(P.x[0], this.F.Fp.mul(9n, P.x[1])),
+//     //         P.x[1],
+//     //     ];
+//     //     let ycoeffs = [
+//     //         this.F.Fp.sub(P.y[0], this.F.Fp.mul(9n, P.y[1])),
+//     //         P.y[1],
+//     //     ];
+//     //     // Isomorphism into subfield of Fp[X]/(w¹² - 18·w⁶ + 82),
+//     //     // where w⁶ = X
+//     //     let nx = [xcoeffs[0], 0n, 0n, 0n, 0n, 0n, xcoeffs[1]];
+//     //     let ny = [ycoeffs[0], 0n, 0n, 0n, 0n, 0n, ycoeffs[1]];
+
+//     //     // Divide x coord by w² and y coord by w³
+//     //     let x = this.F.div(nx, w2);
+//     //     let y = this.F.div(ny, w3);
+
+//     //     return { x, y };
+//     // }
+// }
--- src/pairing.ts
+++ src/pairing.ts
@@ -1,52 +1,144 @@
-// import { Point, EllipticCurve } from "./elliptic_curves";
+import {
+    PointOverFp,
+    PointOverFq,
+    EllipticCurveOverFp,
+    EllipticCurveOverFq,
+} from "./ellipticCurve";
+import { ExtensionField } from "./extensionField";
+import { PrimeField } from "./primeField";
 
-const ATE_LOOP_COUNT = 29793968203157093288;
-const LOG_ATE_LOOP_COUNT = 63;
+// Find line y = mx + c passing through two points P and Q
+// or vertical line y = x0 if Q = -P
+// and evaluate it at a point T
+function line(
+    P: PointOverFq,
+    Q: PointOverFq,
+    T: PointOverFq,
+    Fq: ExtensionField,
+    E: EllipticCurveOverFq
+): bigint[] {
+    // Should we check that P, Q, T are on the curve?
 
-// Create a function representing the line between P1 and P2,
-// and evaluate it at T
-// function line(P1: Point, P2: Point, T: Point, E: EllipticCurve): Point {
-//     if (E.is_zero(P1) || E.is_zero(P2) || E.is_zero(T)) {
-//         throw new Error("Cannot evaluate line at zero");
-//     }
+    if (E.is_zero(P) || E.is_zero(Q) || E.is_zero(T)) {
+        throw new Error("Cannot evaluate line at zero");
+    }
 
-//     // if (P1.x !== P2.x) {
-//     //     const m = (P2.y - P1.y) / (P2.x - P1.x);
-//     //     return m * (T.x - P1.x) - (T.y - P1.y);
-//     // } elif (P1.y === P2.y) {
-//     //     const m = (3n * P1.x * P1.x + E.a) / (2n * P1.y);
-//     //     return m * (T.x - P1.x) - (T.y - P1.y);
-//     // } else {
-//     //     return T.y - P1.y;
-//     // }
+    // First case: P and Q are distinct and not on the same vertical line
+    if (P.x !== Q.x) {
+        const m = Fq.div(Fq.sub(Q.y, P.y), Fq.sub(Q.x, P.x));
+        const c = Fq.sub(P.y, Fq.mul(m, P.x));
+        return Fq.sub(T.y, Fq.add(Fq.mul(m, T.x), c));
 
-//     // const m = (P2.y - P1.y) / (P2.x - P1.x);
-//     // const b = P1.y - m * P1.x;
-//     // return m * T + b;
-// }
+        // Second case: P and Q are the same point
+    } else if (P.y === Q.y) {
+        const m = Fq.div(
+            Fq.add(Fq.mul([3n], Fq.mul(P.x, P.x)), E.a),
+            Fq.mul([2n], P.y)
+        );
+        const c = Fq.sub(P.y, Fq.mul(m, P.x));
+        return Fq.sub(T.y, Fq.add(Fq.mul(m, T.x), c));
 
-// Miller loop
-// function miller_loop(P: Point, Q: Point): Point {
-//     let R = Q;
-//     let f = FQ12.one;
-//     for (let i = LOG_ATE_LOOP_COUNT - 1; i >= 0; i--) {
-//         f = f.square();
-//         f = f.mul(line(R, R, Q));
-//         R = R.double();
-//         if (ATE_LOOP_COUNT & (1n << BigInt(i))) {
-//             f = f.mul(line(R, P, Q, E));
-//             R = R.add(P);
-//         }
-//     }
-//     return f;
-// }
+        // Third case: P and Q are distinct and on the same vertical line
+        // The line is y = P.xy()[0]
+    } else {
+        return Fq.sub(T.y, P.x);
+    }
+}
+
+// This is the BKLS-GHS version of Miller's algorithm for computing the Weil and Tate pairings
+// Note: It only works for even embedding degrees k
+// r is assumed to be in binary form as r[i]
+// For sure, P is assumed to be from the (only) subgroup of E[r] over F_q
+// Q is assumed to be from the trace zero subgroup of E[r] over F_{q**k}
+// i.e., a Type 3 pairing is assumed
+function Miller_loop(
+    P: PointOverFq, // this point is actually over Fp
+    Q: PointOverFq,
+    r: bigint,
+    Fq: ExtensionField,
+    E: EllipticCurveOverFq
+): bigint[] {
+    if (E.is_zero(P) || E.is_zero(Q)) {
+        return Fq.one;
+    }
+
+    let R = P;
+    let f = Fq.one;
+    // Only loop until the second to last bit of r
+    for (let i = log2(r) - 2; i > 0; i--) {
+        f = Fq.mul(Fq.mul(f, f), line(R, R, Q, Fq, E));
+        R = E.add(R, R);
+        if (r & (1n << BigInt(i))) {
+            f = Fq.mul(f, line(R, P, Q, Fq, E));
+            R = E.add(R, P);
+        }
+    }
+
+    f = Fq.mul(Fq.mul(f, f), line(R, R, Q, Fq, E));
+
+    return f;
+}
 
 // Pairing computation
-// function pairing(P: Point, Q: Point): Point {
-//     return miller_loop(P, Q).inverse();
-// }
+function Tate(
+    P: PointOverFq,
+    Q: PointOverFq,
+    r: bigint,
+    Fq: ExtensionField,
+    E: EllipticCurveOverFq
+): bigint[] {
+    // Let's compute the final exponentiation
+    const k = E.embedding_degree(r);
+    const exponent = (r ** k - 1n) / r; // It should be divisible
+    return Fq.exp(Miller_loop(P, Q, r, Fq, E), exponent);
+}
 
-// // Final exponentiation
-// function final_exponentiation(f: Point): Point {
-//     return f.pow((p ** 12 - 1n) / r);
-// }
+function log2(x: bigint): number {
+    if (x == 0n) return 0;
+
+    let r = 1;
+    while (x > 1n) {
+        x = x >> 1n;
+        r += 1;
+    }
+    return r;
+}
+
+const p = 47n;
+const Fp = new PrimeField(p);
+const E = new EllipticCurveOverFp(21n, 15n, Fp);
+const n = 51;
+// the torsion group parameter r is typically chosen
+// as the largest prime factor of the order of the curve
+const r = 17n;
+const k = E.embedding_degree(r);
+console.log("k = ", k);
+
+const P = { x: 45n, y: 23n };
+console.log("rP = ", E.escalarMul(P, r)); // P is in the r-torsion subgroup
+
+// To define Q, we need to move to the extension field F_{p**k}
+const Fq = new ExtensionField(Fp, [5n, 0n, -4n, 0n, 1n]);
+const eE = new EllipticCurveOverFq([21n], [15n], Fq);
+
+const Q = { x: [29n, 0n, 31n], y: [0n, 11n, 0n, 35n] };
+console.log("rQ = ", eE.escalarMul(Q, r)); // Q is in the r-torsion subgroup
+
+// const f = Tate(P, Q, r, Fq, eE);
+
+// assert Tate(P,Q) == 33*u^3 + 43*u^2 + 45*u + 39
+
+// // For sure, the pairing is non-degenerate
+// assert P.additive_order() == Q.additive_order() == r
+// assert Tate(P,Q) != F.one()
+
+// // Let's check the bilinearity of the pairing
+// assert Tate(2*P, 12*Q) == Tate(P,12*Q)^2 == Tate(2*P,Q)^12 == Tate(P,Q)^24 == Tate(12*P,2*Q)
+// // assert Weil(2*P, 12*Q) == Weil(P,12*Q)^2 == Weil(2*P,Q)^12 == Weil(P,Q)^24 == Weil(12*P,2*Q)
+
+// // Check the trivial evaluations are satisfied
+// assert Tate(E(0),Q) == Tate(P,E(0)) == F.one()
+
+// // Since P and Q are generators, we should have that Tate(P,Q) is a primitive r-th root of unity
+// // i.e. a generator of set of roots of unity of order r
+// assert multiplicative_order(Tate(P,Q)) == r

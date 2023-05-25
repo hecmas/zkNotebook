1bf7b60a1a6b4194d8c96cc9501e38dd620f16fc
hecmas <lecter1729@gmail.com>
2023-05-25 17:07:43 +0200

    Added ZeroCheck protocol

--- build/src/multivariate/sumcheck.js
+++ build/src/multivariate/sumcheck.js
@@ -1,21 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.SumCheckProtocol = void 0;
 const multivariatePolynomialRing_1 = require("./multivariatePolynomialRing");
+const utils_1 = require("./utils");
 const bigintRnd = require("bigint-rnd"); // 0 <= bigintRnd(n) < n
-// from LSB to MSB
-function to_bits(n, l) {
-    let bits = [];
-    for (let i = 0; i < l; i++) {
-        bits.push(n & 1n);
-        n >>= 1n;
-    }
-    const diff = l - bits.length;
-    for (let i = 0; i < diff; i++) {
-        bits.push(0n);
-    }
-    return bits;
-}
-// console.log(to_bits(4n, 6));
 class Prover {
     Fp;
     MPRp;
@@ -23,7 +11,7 @@ class Prover {
     pols;
     verifier_challenges;
     round;
-    S;
+    // readonly S: bigint;
     verbose;
     // Constructor
     constructor(p, pol, verbose = false) {
@@ -36,20 +24,20 @@ class Prover {
         this.verifier_challenges = [];
         this.round = 1;
         this.verbose = verbose;
-        let S = 0n;
-        for (let i = 0; i < 2 ** this.nvars; i++) {
-            const input = to_bits(BigInt(i), this.nvars);
-            const summand = MPRp.eval(pol, input);
-            S = Fp.add(S, summand);
-        }
-        this.S = S;
+        // let S = 0n;
+        // for (let i = 0; i < 2 ** this.nvars; i++) {
+        //     const input = to_bits(BigInt(i), this.nvars);
+        //     const summand = MPRp.eval(pol, input);
+        //     S = Fp.add(S, summand);
+        // }
+        // this.S = S;
     }
     compute_and_send_next_polynomial(V) {
         const pol = this.pols[this.pols.length - 1];
         const pad = this.nvars - this.round;
         let g_j = new multivariatePolynomialRing_1.ArrayMap([[[], 0n]]);
         for (let i = 0; i < 2 ** pad; i++) {
-            const input = to_bits(BigInt(i), pad);
+            const input = (0, utils_1.to_bits)(BigInt(i), pad);
             const summand = this.MPRp.eval_symbolic(pol, input);
             g_j = this.MPRp.add(g_j, summand);
         }
@@ -97,9 +85,6 @@ class Verifier {
         this.prover_pols = [];
         this.round = 1;
         this.verbose = verbose;
-        if (this.verbose) {
-            console.log(`Starting sumcheck protocol to prove that the sum of ${this.MPRp.toString(this.pol)} over the ${this.nvars}-dimensionsal boolean hypercube evaluates to ${S}`);
-        }
     }
     receive_polynomial(p) {
         this.prover_pols.push(p);
@@ -118,7 +103,8 @@ class Verifier {
         const deg_gj = (0, multivariatePolynomialRing_1.degree)(gj);
         const deg_j_g = (0, multivariatePolynomialRing_1.degree_j)(this.pol, this.nvars - (this.round - 1));
         if (deg_gj > deg_j_g) {
-            throw new Error(`Prover sent a polynomial of degree ${deg_gj} in round ${this.round}, but the degree should be at most ${deg_j_g}`);
+            console.log(`Prover sent a polynomial of degree ${deg_gj} in round ${this.round}, but the degree should be at most ${deg_j_g}`);
+            return false;
         }
         const sum = this.Fp.add(this.MPRp.eval(gj, [0n]), this.MPRp.eval(gj, [1n]));
         let result;
@@ -130,24 +116,28 @@ class Verifier {
             result = this.MPRp.eval(this.prover_pols[this.prover_pols.length - 2], [challenge]);
         }
         if (this.Fp.neq(sum, result)) {
-            throw new Error(`Prover sent incorrect polynomial in round ${this.round} whose sum is ${sum} but should be ${result}`);
+            console.log(`Prover sent incorrect polynomial in round ${this.round} whose sum is ${sum} but should be ${result}`);
+            return false;
         }
+        if (this.verbose) {
+            console.log(`Polynomial g_${this.round}(X) = ${this.MPRp.toString(gj, true)} is correct`);
+        }
+        return true;
     }
     check_last_polynomial_is_correct() {
         if (this.nvars - 1 !== this.challenges.length) {
-            throw new Error(`Verifier has sent ${this.challenges.length} challenges but should have sent ${this.nvars}`);
+            console.log(`Verifier has sent ${this.challenges.length} challenges but should have sent ${this.nvars}`);
+            return false;
         }
         const r = bigintRnd(this.Fp.p);
         this.challenges.push(r);
         const g_eval = this.MPRp.eval(this.pol, this.challenges.reverse());
         const gv_eval = this.MPRp.eval(this.prover_pols[this.prover_pols.length - 1], [r]);
         if (this.Fp.neq(g_eval, gv_eval)) {
-            throw new Error(`Verifier sent incorrect final polynomial whose evaluation is ${gv_eval} but should be ${g_eval}`);
+            console.log(`Verifier sent incorrect final polynomial whose evaluation is ${gv_eval} but should be ${g_eval}`);
+            return false;
         }
-        else {
-            console.log("Verification succeeded");
-            return true;
-        }
+        return true;
     }
 }
 class SumCheckProtocol {
@@ -158,17 +148,20 @@ class SumCheckProtocol {
     verbose;
     completed;
     // Constructor
-    constructor(p, g, verbose = false) {
+    constructor(p, g, S, verbose = false) {
         const nvars = (0, multivariatePolynomialRing_1.count_number_of_variables)(g);
         if (nvars < 1) {
-            throw new Error(`I can't work with a polynomial of less than 1 variables.`);
+            throw new Error(`I can't work with polynomials of less than 1 variables.`);
         }
         this.nvars = nvars;
         this.P = new Prover(p, g, verbose);
-        this.V = new Verifier(p, g, this.P.S, verbose);
+        this.V = new Verifier(p, g, S, verbose);
         this.round = 1;
         this.verbose = verbose;
         this.completed = false;
+        if (this.verbose) {
+            console.log(`Starting the SumCheck protocol to prove the sum of evaluations of ${this.P.MPRp.toString(g)} over the ${this.nvars}-dimensional boolean hypercube is equal to ${S}.`);
+        }
     }
     next_round() {
         const P = this.P;
@@ -176,19 +169,19 @@ class SumCheckProtocol {
         const completed = this.completed;
         if (!completed) {
             P.compute_and_send_next_polynomial(V);
-            V.check_gj_is_correct();
+            const gjcorrect = V.check_gj_is_correct();
+            if (!gjcorrect) {
+                return false;
+            }
             if (this.nvars === this.round) {
                 const result = V.check_last_polynomial_is_correct();
-                if (result) {
-                    this.completed = true;
-                }
-                else {
-                    throw new Error(`Verification failed`);
-                }
+                this.completed = true;
+                return result && true;
             }
             else {
                 this.V.compute_and_send_challenge(P);
                 this.round += 1;
+                return true;
             }
         }
         else {
@@ -197,16 +190,27 @@ class SumCheckProtocol {
     }
     run_protocol() {
         while (!this.completed) {
-            this.next_round();
+            const round_result = this.next_round();
+            if (!round_result) {
+                if (this.verbose) {
+                    console.log(`SumCheck failed in round ${this.round}`);
+                }
+                return false;
+            }
         }
+        if (this.verbose) {
+            console.log(`SumCheck succeeded.`);
+        }
+        return true;
     }
 }
+exports.SumCheckProtocol = SumCheckProtocol;
 // Main
-const p = 97n;
-const g = new multivariatePolynomialRing_1.ArrayMap();
-g.set([3, 0, 0], 2n);
-g.set([1, 0, 1], 1n);
-g.set([0, 1, 1], 1n);
-const SumCheck = new SumCheckProtocol(p, g, true);
-SumCheck.run_protocol();
+// const p = 97n;
+// const g = new ArrayMap();
+// g.set([3, 0, 0], 2n);
+// g.set([1, 0, 1], 1n);
+// g.set([0, 1, 1], 1n);
+// const SumCheck = new SumCheckProtocol(p, g, 12n, true);
+// const result = SumCheck.run_protocol();
 //# sourceMappingURL=sumcheck.js.map
